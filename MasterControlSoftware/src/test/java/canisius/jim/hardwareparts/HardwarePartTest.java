1a55f65fd2c7231768f1ea18f63ac4adf8e9a4d7
JonMrowczynski <jonmrowczynski@gmail.com>
2026-05-25 00:51:45 -0400

    Fixed HardwarePartTest tests.

--- MasterControlSoftware/src/test/java/canisius/jim/hardwareparts/HardwarePartTest.java
+++ MasterControlSoftware/src/test/java/canisius/jim/hardwareparts/HardwarePartTest.java
@@ -41,30 +41,31 @@ abstract class HardwarePartTest {
 	protected HardwarePart hardwarePart;
 	
 	@Test void setNeutral() {
+		final var iterator = hardwarePart.getNeutralState().iterator();
 		// A newly instantiated Part's neutral state should be default_neutral.
-		assertEquals(default_neutral, hardwarePart.getNeutralState().iterator().next().getData2());
+		assertEquals(default_neutral, iterator.next().getMessage()[2]);
 		
 		// Setting the neutral state below the lower bound should result in the neutral state being set to the lower
 		// bound.
 		hardwarePart.setNeutral(HardwarePart.MIN_BOUND - 1);
-		assertEquals(HardwarePart.MIN_BOUND, hardwarePart.getNeutralState().iterator().next().getData2());
+		assertEquals(HardwarePart.MIN_BOUND, iterator.next().getMessage()[2]);
 		
 		// Setting the neutral state above the upper bound should result in the neutral state being set to the upper
 		// bound.
 		hardwarePart.setNeutral(HardwarePart.MAX_BOUND + 1);
-		assertEquals(HardwarePart.MAX_BOUND, hardwarePart.getNeutralState().iterator().next().getData2());
+		assertEquals(HardwarePart.MAX_BOUND, iterator.next().getMessage()[2]);
 		
 		// Setting the neutral state to any valid value should result in a successful modification of the neutral
 		// state.
 		final var average = (HardwarePart.MAX_BOUND + HardwarePart.MIN_BOUND) / 2;
 		hardwarePart.setNeutral(average);
-		assertEquals(average, hardwarePart.getNeutralState().iterator().next().getData2());
+		assertEquals(average, iterator.next().getMessage()[2]);
 	}
 	
 	@Test void addStateToTrack() throws InvalidMidiDataException {
+		final var messages = hardwarePart.getLowerBoundState();
 		// Adding any state to a null Track should throw a NullPointerException.
-		assertThrows(NullPointerException.class,
-		             () -> hardwarePart.addStateToTrack(null, hardwarePart.getLowerBoundState(), -1));
+		assertThrows(NullPointerException.class, () -> hardwarePart.addStateToTrack(null, messages, -1));
 		
 		// Adding a null Set of ShortMessages should result in a still empty Track.
 		final var sequence = new Sequence(Sequence.PPQ, 160);
@@ -74,9 +75,8 @@ abstract class HardwarePartTest {
 		
 		// Adding a singleton Set of a ShortMessage to a Track should contain the data of the ShortMessage with the
 		// maximum tick.
-		hardwarePart.addStateToTrack(track, hardwarePart.getLowerBoundState(), 10);
-		assertEquals(hardwarePart.getLowerBoundState().iterator().next().getData2(),
-		             track.get(0).getMessage().getMessage()[2]);
+		hardwarePart.addStateToTrack(track, messages, 10);
+		assertEquals(messages.iterator().next().getMessage()[2], track.get(0).getMessage().getMessage()[2]);
 		assertEquals(2, track.size()); // Contains two events. The one we added and the eotEvent.
 		assertEquals(10, track.ticks());
 		
@@ -90,65 +90,69 @@ abstract class HardwarePartTest {
 	}
 	
 	@Test void getUpperBoundState() {
+		final var messages = hardwarePart.getUpperBoundState();
 		// The upper bound state of a Part should be Part.MAX_BOUND.
-		assertEquals(HardwarePart.MAX_BOUND, hardwarePart.getUpperBoundState().iterator().next().getData2());
+		assertEquals(HardwarePart.MAX_BOUND, messages.iterator().next().getMessage()[2]);
 		
 		// The Part's upper bound state should only contain one element.
-		assertEquals(1, hardwarePart.getUpperBoundState().size());
+		assertEquals(1, messages.size());
 	}
 	
 	@Test void getLowerBoundState() {
 		// The lower bound state of a Part should be Part.MIN_BOUND.
-		final var iterator = hardwarePart.getLowerBoundState().iterator();
-		assertEquals(HardwarePart.MIN_BOUND, iterator.next().getData2());
+		final var messages = hardwarePart.getLowerBoundState();
+		assertEquals(HardwarePart.MIN_BOUND, messages.iterator().next().getMessage()[2]);
 		
 		// The Part's lower bound state should only contain one element.
 		assertEquals(1, hardwarePart.getLowerBoundState().size());
 	}
 	
 	@Test void getNeutralState() {
+		final var iterator = hardwarePart.getNeutralState().iterator();
 		// A newly instantiated Part's neutral state should be default_neutral
-		assertEquals(default_neutral, hardwarePart.getNeutralState().iterator().next().getData2());
+		assertEquals(default_neutral, iterator.next().getMessage()[2]);
 		
 		// Setting the neutral state to any valid value should result in a successful modification of the neutral
 		// state.
 		final var average = (HardwarePart.MIN_BOUND + HardwarePart.MAX_BOUND) / 2;
 		hardwarePart.setNeutral(average);
-		assertEquals(average, hardwarePart.getNeutralState().iterator().next().getData2());
+		assertEquals(average, iterator.next().getMessage()[2]);
 	}
 	
 	@Test void getUpperBoundHardwarePartState() {
+		final var hardwarePartState = hardwarePart.getUpperBoundHardwarePartState();
 		// A newly instantiated Part's upper bound PartState should contain itself as the Part.
-		assertSame(hardwarePart, hardwarePart.getUpperBoundHardwarePartState().hardwarePart());
+		assertSame(hardwarePart, hardwarePartState.hardwarePart());
 		
 		// A newly instantiated Part's upper bound PartState should contain Part.MAX_BOUND for its state.
-		assertEquals(HardwarePart.MAX_BOUND,
-		             hardwarePart.getUpperBoundHardwarePartState().state().iterator().next().getData2());
+		assertEquals(HardwarePart.MAX_BOUND, hardwarePartState.state().iterator().next().getMessage()[2]);
 		
 		// A newly instantiated Part's upper bound PartState should contain only one element.
-		assertEquals(1, hardwarePart.getUpperBoundHardwarePartState().state().size());
+		assertEquals(1, hardwarePartState.state().size());
 	}
 	
 	@Test void getLowerBoundHardwarePartState() {
+		final var hardwarePartState = hardwarePart.getLowerBoundHardwarePartState();
 		// A newly instantiated Part's lower bound PartState should contain itself as the Part.
-		assertSame(hardwarePart, hardwarePart.getLowerBoundHardwarePartState().hardwarePart());
+		assertSame(hardwarePart, hardwarePartState.hardwarePart());
 		
 		// A newly instantiated Part's lower bound PartState should contain Part.MIN_BOUND for its state.
-		assertEquals(HardwarePart.MIN_BOUND,
-		             hardwarePart.getLowerBoundHardwarePartState().state().iterator().next().getData2());
+		assertEquals(HardwarePart.MIN_BOUND, hardwarePartState.state().iterator().next().getMessage()[2]);
 		
 		// A newly instantiated Part's lower bound PartState should contain only one element.
-		assertEquals(1, hardwarePart.getLowerBoundHardwarePartState().state().size());
+		assertEquals(1, hardwarePartState.state().size());
 	}
 	
 	@Test void getNeutralHardwarePartState() {
+		final var hardwarePartState = hardwarePart.getNeutralHardwarePartState();
 		// A newly instantiated Part's neutral PartState should contain itself as the Part.
-		assertSame(hardwarePart, hardwarePart.getNeutralHardwarePartState().hardwarePart());
+		assertSame(hardwarePart, hardwarePartState.hardwarePart());
 		
+		final var state = hardwarePartState.state();
 		// A newly instantiated Part's neutral PartState should contain default_neutral for its state.
-		assertEquals(default_neutral, hardwarePart.getNeutralHardwarePartState().state().iterator().next().getData2());
+		assertEquals(default_neutral, state.iterator().next().getMessage()[2]);
 		
 		// A newly instantiated Part's neutral PartState should contain only one element.
-		assertEquals(1, hardwarePart.getNeutralHardwarePartState().state().size());
+		assertEquals(1, state.size());
 	}
 }
